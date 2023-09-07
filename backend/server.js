5cbd8271d5ad8601c81aa5524f5f288095fb6213
s123tuti <stutisharmaofficial@gmail.com>
2023-09-07 16:10:40 +0530

    done

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,22 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
 const Product = require('./models/product');
-
-dotenv.config();
 const app = express();
-const port = process.env.PORT || 3000;
-const dbURI = process.env.MONGODB_URI;
 
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log('Connected to MongoDB');
-    app.listen(port, () => {
-      console.log(`Server is running on port ${port}`);
-    });
-  })
+
+mongoose.connect("mongodb+srv://stuti3007:w14E1dmx6wAE1h7i@cluster0.rrvbnsb.mongodb.net/goodshelf" , { 
+  useNewUrlParser: true, 
+  useUnifiedTopology: true 
+})
+
+  .then(() =>console.log('Connected to MongoDB'))
   .catch((err) => {
     console.error('Error connecting to MongoDB:', err);
   });
+  app.listen(process.env.PORT || 3000, function(){
+    console.log("express is running port" + (process.env.PORT||3000))
+
+  } )
 
 //   API route for fetching the data......
 
